07ca75c2497a2bbe8f2674972f87773c160a7f41
Ali Ruzmetov <ruzmetovali78@gmail.com>
2025-04-18 15:15:59 +0500

    script

---
+++ app.js
@@ -0,0 +1,55 @@
+// // alert(1);
+
+
+//  let car1 = "Volvo"; 
+//  let car2 = "Jentra"; 
+//  let car3 = "Damas" ;
+
+
+// let cars = ["Volvo","Jentra","Damas"];
+// cars[3] = "matiz"
+// cars[4] = "cobalt"
+// cars[6] = "Nexia"
+
+
+// console.log(cars.length);
+// console.log(cars);
+
+
+// let letters =["a","b","c","d","e"];
+// let raqamlar = [0,1,2,3,4,5,6];
+// console.log(letters);
+// console.log(raqamlar);
+
+// console.log(letters[0]);
+// console.log(letters[1]);
+// console.log(letters[2]);
+// console.log(letters[3]);
+// console.log(letters[4]);
+
+// // let student1 ="Davaltyar";
+// // let student2 ="Behruz";
+// // let student3 ="Ismoil";
+// // let student4 ="Islom";
+// // let student5 ="Sunnat";
+// // let student6 ="Bekzod";
+// // let student7 ="jumayoz";
+// // let student8 ="ismoil.P";
+// // let student9 ="X";
+// // let student10 ="Ali";
+// let students = ["behruzz","Davaltyar","Behruz","Ismoil","Islom","Sunnat","Bekzod","Jumayoz","Ali","x","Ismoil.p"]
+// console.log(students);
+// console.log(students.length);
+// for (  i = 0; i<= 10; i++) {
+//     console.log("Salom", students[i], );
+    
+// }
+
+
+let fruits = ["Apple", "Banana"];
+fruits.push("kiwi");
+fruits.push("strawbery");
+fruits.push("melon");
+fruits[3] = ("Dragon Fruits");
+fruits.pop(2);
+console.log(fruits[2]);
