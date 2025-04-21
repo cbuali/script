8d0aaed5f42304f7184f2a10e30c8d0046ef6b9f
Ali Ruzmetov <ruzmetovali78@gmail.com>
2025-04-21 16:18:50 +0500

    first commit

--- app.js
+++ app.js
@@ -37,19 +37,121 @@
 // // let student8 ="ismoil.P";
 // // let student9 ="X";
 // // let student10 ="Ali";
-// let students = ["behruzz","Davaltyar","Behruz","Ismoil","Islom","Sunnat","Bekzod","Jumayoz","Ali","x","Ismoil.p"]
-// console.log(students);
-// console.log(students.length);
-// for (  i = 0; i<= 10; i++) {
-//     console.log("Salom", students[i], );
-    
-// }
 
 
-let fruits = ["Apple", "Banana"];
-fruits.push("kiwi");
-fruits.push("strawbery");
-fruits.push("melon");
-fruits[3] = ("Dragon Fruits");
-fruits.pop(2);
-console.log(fruits[2]);
+// let fruits = ["Apple", "Banana"];
+// fruits.push("kiwi");
+// fruits.push("strawbery");
+// fruits.push("melon");
+// fruits[3] = ("Dragon Fruits");
+// fruits.pop(2);
+// console.log(fruits[2]);
+
+
+//  let raqamlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // for (let i = 0; i < raqamlar.length; i++) {
+    // //     if(raqamlar[i] % 2 === 1) {
+        // //         console.log(raqamlar[i]);
+        
+        // //     }
+        // // }  (let i = 0 ;<raqamlar.length; i++) {
+            // //     if (condition) {
+                
+            // //     } else {
+                
+            // //     } (raqamlar[i] & 2 === 0) {
+                // //         console.log(raqamlar[i]);
+                
+                // //     }
+                // // }
+                
+                
+                // // let toq = [];
+                // // let juft = []; 
+                // // for (let i = 0; i < raqamlar.length; i++) {
+                    // //     if (raqamlar[i] % 2 === 0) {
+                        // //         juft.push(raqamlar [i]);
+                        // //     }else {
+                            // //         toq.push(raqamlar[i]);
+                            // //     }
+                            // // }
+                            // // console.log(juft);
+                            // // console.log(toq);
+                            // // Bu 1 dan 10gacha bo'lgan sonlarni toq va juft sonlarni chiqarish/ 
+                            
+                            // let aralash = [1, 2, 3, "matn", "qwerty", false];
+                            
+                            // let sonlar = [1, 20, 5, 8, 4, 9];
+                            // let summa = 0;
+                            
+                            // for (let i = 0; i < sonlar.length; i++) {
+                                //     summa += sonlar[i];
+                                
+
+                            // }
+                            // console.log("Yig'indisi:", summa);
+                            // bU JAMI QOSHILGAN SONLAR YIG'INDISI
+                            // let students = ["behruzz","Davaltyar","Behruz","Ismoil","Islom","Sunnat","Bekzod","Jumayoz","Ali","x","Ismoil.p" ]
+                            
+                            // console.log(students.length);
+                            //             for (  i = 0; i<= 10; i++) {     console.log("Salom", students[i], );
+                    // Brand -- "android"
+                    // model -- "A 10"
+                    // color -- "lightblue"
+                    // price -- "150$"
+                    // // is_apple -- false
+                    // aliandroid {
+                    //    brand:"Android",
+                    //    model:"a10",
+                    //    color:lightblue,
+                    //    price:150$,
+                    //    is_apple;"false," 
+                    // }
+ 
+
+                //     let aliphone = {
+                //         brand:"Android",
+                //         price: 150,
+                //         model:"A 10 ",
+                //         locations:["home","school","street"],
+                //         gps:true,
+                //         isNew:false,
+                //     };
+                //   console.log("brand:", aliphone.brand);
+                //   console.log("price:", aliphone.price);
+                //   console.log("model:", aliphone.model);
+                //   console.log("locations:", aliphone.locations);
+                //   console.log("gps:", aliphone.gps);
+                //   console.log("isNew:", isNew.gps);
+                  let odom1 = {
+                    name:"ali",
+                    surname:"Ruzmetov",
+                    age:15,
+                    can_drive:true,
+                    has_gentra:true,
+                    subjects:["english,PE"],
+
+                  };
+                 console.log("Ism:",odom1.name);
+                 console.log("Familyav:",odom1.surname);
+                 console.log("Yosh:",odom1.age);
+                 console.log("Hayday olami?:",odom1.can_drive);
+                 console.log("Gentrasi bormi?:",odom1.has_gentra);
+                 console.log("Fanlari:",odom1.subjects);
+                  
+                 let odom2 = {
+                    name:"behruz",
+                    surname:"Ruzmetov",
+                    age:4,
+                    can_drive:false,
+                    has_gentra:true,
+                    subjects:["english,PE"],
+
+                  };
+                 console.log("Ism:",odom2.name);
+                 console.log("Familyav:",odom2.surname);
+                 console.log("Yosh:",odom2.age);
+                 console.log("Hayday olami?:",odom2.can_drive);
+                 console.log("Gentrasi bormi?:",odom2.has_gentra);
+                 console.log("Fanlari:",odom2.subjects);
+                  
